b88ce078189e8c5bb8009b812c4ad1ba3a1a6c63
Ryan Spears <sperj001@hotmail.com>
2015-01-30 07:34:54 +1030

    Initial working hookup of angular

--- compile-template/app.js
+++ compile-template/app.js
@@ -1,9 +1,10 @@
 /**
  * Created by Ryan on 30/01/2015.
  */
-angular.module('website', [])
+angular.module('MainApp', [])
     .controller('MainCtrl', ['$scope', 'CompileService', function ($scope, CompileService) {
         var vm = this;
+        vm.title = "Ryan Spears";
 
        return vm;
     }])
